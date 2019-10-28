ae91ac36202432f23a9f6b5daadb2a3553e2a480
Andrea Bender <andreab.1726@gmail.com>
2019-10-28 14:12:53 -0600

    drill2 completed

--- drill2.js
+++ drill2.js
@@ -1,3 +1,25 @@
+/* eslint-disable no-console */
+/* eslint-disable strict */
 const express = require('express');
 const app = express();
 
+app.get('/cipher', (req, res) => {
+  const text = req.query.text;
+  const shift = req.query.shift;
+ 
+  const textArr = text.split('');
+  let result = textArr.map(letter => {
+    const num = parseInt(shift);
+    const letterNum = letter.charCodeAt(0);
+    if(letterNum === 90){
+      return String.fromCharCode(65);
+    }
+    return String.fromCharCode(letterNum + num);  
+  });
+
+  res.send(result.join('')); 
+});
+
+app.listen(8000, () => {
+  console.log(String.fromCharCode(65));
+});
